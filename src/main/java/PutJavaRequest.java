3e9b21309f89b5ea3bb9245cfd2a4741f3a4faa5
volgofant <viktor.nenashev@westwing.ru>
2020-01-09 10:28:28 +0300

    first commit

---
+++ src/main/java/PutJavaRequest.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParseForJavaRequest {
+    public ParseForJavaRequest(int fileName, int quantity) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\viktor.nenashev\\sku\\" + fileName + ".csv"));
+        String line;
+        List<String> lines = new ArrayList<String>();
+        while ((line = reader.readLine()) != null) {
+            lines.add(line);
+        }
+        for(String s : lines) {
+            new ApiRequest(s, quantity);
+        }
+    }
+}
